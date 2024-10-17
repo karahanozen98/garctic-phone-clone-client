cf92ccec2e38ba1ab065e8f42b2137eed7453d6e
Karahan Özen <karahan.ozen@adesso.com.tr>
2024-10-17 16:43:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import "./App.css";
+import { MODES } from "./constants";
+import { useWindowSize } from "./hooks";
+import Canvas from "./canvas";
+import { socket } from "./socket";
 
 function App() {
+  const settings = useRef({
+    stroke: 3,
+    color: "#000",
+    mode: MODES.PEN,
+  });
+
+  const size = useWindowSize();
+
+  useEffect(() => {
+    function onConnect() {
+      console.log(true);
+    }
+
+    function onDisconnect() {
+      console.log(true);
+    }
+
+    function onFooEvent(value) {
+      console.log(true);
+    }
+
+    fetch("http://localhost:4000").then((data) =>
+      data.json().then((json) => console.log(json))
+    );
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="canvas-container">
+        <Canvas width={500} height={500} settings={settings} />
+      </div>
     </div>
   );
 }
