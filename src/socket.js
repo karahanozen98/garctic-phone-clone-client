eba063ba0ae6c1347eef44e528fe10a18fd61eb1
Karahan Özen <karahan.ozen@adesso.com.tr>
2024-10-20 13:43:22 +0300

    add credentials to socketio

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,6 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.REACT_APP_BACKEND_URL;
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  withCredentials: true,
+});
